c51590891eb6633458fa0a94d94dc68e66c51416
penicillin0 <ayahitonakamura@gmail.com>
2025-02-13 10:48:53 +0900

    reset

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,9 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
-import config from "../tamagui.config";
-
-import { TamaguiProvider } from "tamagui";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -52,11 +44,9 @@ export default function RootLayout() {
 
 function RootLayoutNav() {
   return (
-    <TamaguiProvider config={config}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-      </Stack>
-    </TamaguiProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+    </Stack>
   );
 }
--- app/modal.tsx
+++ app/modal.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { Platform, StyleSheet } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 
-import { Text, View } from "tamagui";
+import EditScreenInfo from "@/components/EditScreenInfo";
 
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Modal</Text>
       <View style={styles.separator} />
+      <EditScreenInfo path="app/modal.tsx" />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
